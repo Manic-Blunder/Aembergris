3dea922407b8b8740cd11afdbb38b137f78476f2
Bruce Lindman <thegnerg@gmail.com>
2019-09-26 18:21:05 -0400

    archons controller set up

--- app/controllers/archons_controller.rb
+++ app/controllers/archons_controller.rb
@@ -1,2 +1,26 @@
 class ArchonsController < ApplicationController
+
+  # GET lodges/:lodge_id/archons
+  def index
+    @lodge = Lodge.find params[:lodge_id]
+    @archons = Lodge.archons
+  end
+
+  # GET lodges/:lodge_id/archons/:id
+  def show
+    @lodge = Lodge.find params[:lodge_id]
+    @archon = @lodge.archons.find params[:id]
+  end
+
+  # POST lodges/:lodge_id/archons/
+  def create
+    #   This will actually call the import logic
+  end
+
+  # DELETE lodges/:lodge_id/archons/:id
+  def delete
+    @lodge = Lodge.find params[:lodge_id]
+    @lodge.archons.find(params[:id]).destroy
+  end
+
 end
