3074f81db28156d826d906e75578cef009c91d08
Bruce Lindman <thegnerg@gmail.com>
2019-10-01 19:37:52 -0400

    include houses in archon jsonification

--- app/models/archon.rb
+++ app/models/archon.rb
@@ -1,4 +1,8 @@
 class Archon < ApplicationRecord
   belongs_to :lodge
   has_and_belongs_to_many :houses
+
+  def as_json(options = {})
+    super(except: %i[created_at updated_at], include: :houses)
+  end
 end
