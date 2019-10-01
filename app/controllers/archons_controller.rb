2cb2fbd8438358727e74531a449510a6a73888f9
Bruce Lindman <thegnerg@gmail.com>
2019-10-01 18:19:14 -0400

    changing the import just a tad

--- app/controllers/archons_controller.rb
+++ app/controllers/archons_controller.rb
@@ -12,19 +12,18 @@ class ArchonsController < ApplicationController
     render json: @lodge.archons.find(params[:id])
   end
 
-  # POST lodges/:lodge_id/archons/
-  def create
-    import_link = params[:import_link]
-    if import_link.include? 'https://www.keyforgegame.com/deck-details/'
-      import_link.slice! 'https://www.keyforgegame.com/deck-details/'
-      ImportArchon.call(import_link)
-    end
-  end
-
   # DELETE lodges/:lodge_id/archons/:id
   def delete
     @lodge = Lodge.find params[:lodge_id]
     @lodge.archons.find(params[:id]).destroy
+  end
+
+  # @param [Object] import_link
+  def import(import_link)
+    if import_link.include? 'https://www.keyforgegame.com/deck-details/'
+      import_link.slice! 'https://www.keyforgegame.com/deck-details/'
+      ImportArchon.call(import_link)
+    end
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :lodges do
     resources :archons
     resources :battles
+    post '/archons/import', to: 'archons#import'
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
